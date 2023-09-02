3a1bbd31af4de5ffaa3233704093ea0339c631a9
Bluishoul <bluishoul@gmail.com>
2023-09-02 11:24:11 +0800

    feat: add confetti effect on config ready

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import "./globals.css";
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
+import Confetti from "@/components/confetti";
 
 export default function CardWithForm() {
   const configReady: boolean =
@@ -23,31 +22,27 @@ export default function CardWithForm() {
           <CardDescription>Safeguard your money in one-click</CardDescription>
         </CardHeader>
         <CardContent>
-          {
-            // if config is ready, show a All Set message with emoji
-            // else show a red warning message to remind user to set config
-            configReady ? (
-              <div className="flex items-center justify-center">
-                <span className="text-2xl">✅ All Set</span>
-              </div>
-            ) : (
-              <div className="flex items-center justify-center">
-                <span className="text-2xl">⚠️</span>
-                <span className="ml-2">
-                  Please add <code>HF_TOKEN</code> and <code>HF_NAME</code> to{" "}
-                  <Link
-                    href="https://vercel.com/docs/projects/environment-variables"
-                    target="_blank"
-                    className="text-blue-500"
-                  >
-                    Environment Variables
-                  </Link>{" "}
-                  in
-                  vercel project setting
-                </span>
-              </div>
-            )
-          }
+          {configReady ? (
+            <div className="flex items-center justify-center">
+              <Confetti />
+              <span className="text-2xl">🎉 All Set</span>
+            </div>
+          ) : (
+            <div className="flex items-center justify-center">
+              <span className="text-2xl">⚠️</span>
+              <span className="ml-2">
+                Please add <code>HF_TOKEN</code> and <code>HF_NAME</code> to{" "}
+                <Link
+                  href="https://vercel.com/docs/projects/environment-variables"
+                  target="_blank"
+                  className="text-blue-500"
+                >
+                  Environment Variables
+                </Link>{" "}
+                in vercel project setting
+              </span>
+            </div>
+          )}
         </CardContent>
       </Card>
     </div>
---
+++ components/confetti/index.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import confetti from "canvas-confetti";
+
+export default function Confetti() {
+  const ref = useRef(null);
+  useEffect(() => {
+    if (ref.current) {
+      const c = confetti.create(ref.current, {
+        resize: true,
+        useWorker: true,
+      });
+      c({
+        particleCount: 100,
+        spread: 160,
+        origin: { y: 0.6 },
+      });
+    }
+  }, []);
+  return <canvas ref={ref} className="fixed w-full h-full" />;
+}
